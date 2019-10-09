e71b9e6e10188c1e23611d0379da497b9f9c07fd
Air <airzhaoyn@gmail.com>
2019-10-09 13:22:44 +0800

    remove unused modules

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-    initConnection: require('./client').init,
-    setOnOriginDataReceivedCallback: require('./client').setOnOriginDataReceivedCallback,
-    getConnectedModuleNames: require('./client').getConnectedModuleNames,
-    addResetListener: require("./client").addResetListener,
-    clearResetListener: require("./client").clearResetListener,
-    addDisconnectListener: require('./client').addDisconnectListener,
-    addConnectSuccessListener: require('./client').addConnectSuccessListener
+    initConnection: require('./client').default.init,
+    setOnOriginDataReceivedCallback: require('./client').default.setOnOriginDataReceivedCallback,
+    getConnectedModuleNames: require('./client').default.getConnectedModuleNames,
+    addResetListener: require("./client").default.addResetListener,
+    clearResetListener: require("./client").default.clearResetListener,
+    addDisconnectListener: require('./client').default.addDisconnectListener,
+    addConnectSuccessListener: require('./client').default.addConnectSuccessListener
 }
